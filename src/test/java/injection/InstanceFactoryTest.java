524fb2d1146cfae1ad1167c85cb4f01717d6ebd4
AFurber <afurber@thenavisway.com>
2016-11-01 13:59:29 -0700

    Some more tests.

--- src/test/java/injection/InstanceFactoryTest.java
+++ src/test/java/injection/InstanceFactoryTest.java
@@ -28,7 +28,9 @@ public class InstanceFactoryTest {
 
                 try {
                     wait(); // 6 -- release lock and wait until it can be re-obtained to continue
-                } catch (InterruptedException e) { e.printStackTrace(); }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
                 unicorn = factory.make();
             }
@@ -65,10 +67,10 @@ public class InstanceFactoryTest {
 
             // thread 1 has now overridden the unicorn factory with a unicorn poop factory -- makin' rainbows!
 
-            thread2.start(); // thread 2 should be makin' unicorns
-            thread2.join(); // 5 -- wait until thread2 runs to completion
+            synchronized (one) { // 6 -- wait for lock, before notifying thread1 that thread2 is done
+                thread2.start(); // thread 2 should be makin' unicorns
+                thread2.join(); // 5 -- wait until thread2 runs to completion
 
-            synchronized (one){ // 6 -- wait for lock, before notifying thread1 that thread2 is done
                 one.notify(); // 7 -- notify thread1 (thread2 is finished), release lock
             }
         }
@@ -76,6 +78,39 @@ public class InstanceFactoryTest {
 
         assertEquals("Thread one should override with:", rainbows, one.unicorn);
         assertEquals("Thread two should retain factory value with:", unicorn, two.unicorn);
+    }
+
+    @Test
+    public void overrides_the_override_build_function_on_a_particular_thread() throws InterruptedException {
+        final String unicorn = "unicorn";
+        final String rainbows = "unicorn poop";
+        final String glitter = "glitter";
+        final InstanceFactory<String> factory = new InstanceFactory<>(() -> unicorn);
+
+        factory.override(() -> rainbows);
+        final String firstUnicorn = factory.make();
+
+        factory.override(() -> glitter);
+        final String secondUnicorn = factory.make();
+
+        assertEquals("The first override should be:", rainbows, firstUnicorn);
+        assertEquals("The second override should be:", glitter, secondUnicorn);
+    }
+
+    @Test
+    public void pops_to_the_previous_override_value_when_close_is_called() {
+        final String unicorn = "unicorn";
+        final String rainbows = "unicorn poop";
+        final String glitter = "glitter";
+        final InstanceFactory<String> factory = new InstanceFactory<>(() -> unicorn);
+
+        factory.override(() -> rainbows);
+        factory.override(() -> glitter);
+        factory.close();
+
+        final String firstUnicorn = factory.make();
+
+        assertEquals("The override should go back to the first value:", rainbows, firstUnicorn);
     }
 
 }
